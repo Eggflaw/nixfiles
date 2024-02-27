e250979a01bfa389a304099128199a86855607e2
eggflaw <ozzygado127@gmail.com>
2024-02-27 18:23:02 +0900

    Update flake

--- flake.nix
+++ flake.nix
@@ -5,57 +5,48 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-       url = "github:nix-community/home-manager";
-       inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    
   };
 
-  outputs = { self, nixpkgs, home-manager ,... }@inputs:
-    let
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
     inherit (self) outputs;
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    # packages = forAllSystems (system: import ./pkgs {inherit nixpkgs;} );
 
-      systems = [
-      "aarch64-linux"
-      "i686-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
+    nixosSystemModules = import ./modules/system;
 
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      # packages = forAllSystems (system: import ./pkgs {inherit nixpkgs;} );
+    homeModules = import ./modules/home;
 
-      nixosSystemModules = import ./modules/system;
+    overlays = import ./overlays {inherit inputs;};
 
-      homeModules = import ./modules/home;
-
-      overlays = import ./overlays {inherit inputs;};
- 
-      # NixOs configuration 
-      nixosConfigurations = {
-      	nixos = nixpkgs.lib.nixosSystem {
-	      specialArgs = {inherit inputs outputs;};
-	      modules = [
-	   	    ./nixos/configuration.nix
-	      ];
-	      };
+    # NixOs configuration
+    nixosConfigurations = {
+      default = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          ./nixos/configuration.nix
+        ];
       };
-
-      # Home manager configuration
-      homeConfigurations = {
-      "eggflaw" = home-manager.lib.homeManagerConfiguration {
-      	pkgs = pkgs;
-        extraSpecialArgs = {inherit inputs outputs;};
-	      modules = [
-	        ./home/home.nix
-	      ];
-      };
-	};
     };
+
+    # Home manager configuration
+    homeConfigurations = {
+      default = home-manager.lib.homeManagerConfiguration {
+        pkgs = pkgs;
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [
+          ./home/home.nix
+        ];
+      };
+    };
+  };
 }
